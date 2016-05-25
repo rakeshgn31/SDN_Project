26a866bcbf531e2d671a439149d212b096cfbade
niroopskumar <nirup9@gmail.com>
2016-05-25 15:03:47 +0200

    Update alphatopo.py

--- ik2220-assign-team1/topology/alphatopo.py
+++ ik2220-assign-team1/topology/alphatopo.py
@@ -26,7 +26,7 @@ class NetworkTopo( Topo ):
         #Public Zone
         h1 = self.addHost( 'h1', ip='100.0.0.10/24')
         h2 = self.addHost( 'h2', ip='100.0.0.11/24')
-        sw1 = self.addSwitch( 'sw1' )
+        sw1 = self.addSwitch( 'sw1', dpid="0000000000000201" )
         self.addLink( h1, sw1 )
         self.addLink( h2, sw1 )
         
@@ -34,7 +34,7 @@ class NetworkTopo( Topo ):
         ds1 = self.addHost( 'ds1', ip='100.0.0.20/24')
         ds2 = self.addHost( 'ds2', ip='100.0.0.21/24')
         ds3 = self.addHost( 'ds3', ip='100.0.0.22/24' )
-        sw3 = self.addSwitch( 'sw3' )
+        sw3 = self.addSwitch( 'sw3', dpid="0000000000000203" )
         self.addLink( ds1, sw3 )
         self.addLink( ds2, sw3 )
         self.addLink( ds3, sw3 )
@@ -43,7 +43,7 @@ class NetworkTopo( Topo ):
         ws1 = self.addHost( 'ws1', ip='100.0.0.40/24')
         ws2 = self.addHost( 'ws2', ip='100.0.0.41/24')
         ws3 = self.addHost( 'ws3', ip='100.0.0.42/24' )
-        sw4 = self.addSwitch( 'sw4' )
+        sw4 = self.addSwitch( 'sw4', dpid="0000000000000204" )
         self.addLink( ws1, sw4 )
         self.addLink( ws2, sw4 )
         self.addLink( ws3, sw4 )
@@ -56,15 +56,15 @@ class NetworkTopo( Topo ):
         self.addLink( h4, sw5 )
         
         #Border equipment interconnect
-        sw2 = self.addSwitch( 'sw2' )
-        fw1 = self.addSwitch( 'fw1' )
-        fw2 = self.addSwitch( 'fw2' )
+        sw2 = self.addSwitch( 'sw2', dpid="0000000000000202" )
+        fw1 = self.addSwitch( 'fw1', dpid="0000000000000205" )
+        fw2 = self.addSwitch( 'fw2', dpid="0000000000000206" )
         self.addLink( sw1, fw1 )
         self.addLink( fw1, sw2 )
         self.addLink( sw2, fw2 )
         
         #Extra equip
-        testsw = self.addSwitch( 'sw6' )
+        testsw = self.addSwitch( 'sw6', dpid="0000000000000207" )
         inspector = self.addHost( 'insp' )
         
 
